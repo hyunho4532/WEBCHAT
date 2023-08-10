f25df290732f655534ae2f2068939fb2f178144d
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-10 22:49:14 +0900

    2023-08-10 웹소캣 구현

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def echo_page(request):
+    return render(request,  "app/echo_page.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'channels',
     'daphne',
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
