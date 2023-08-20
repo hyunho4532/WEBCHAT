d3b50205865ff001b6f9485d4e7251174b7aecd1
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-21 00:07:31 +0900

    2023-08-21 채팅방 단위로 그룹 채팅 구현

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from chat import views
+
 app_name = "chat"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/chat/", views.room_chat, name="root_chat"),
+    path("new/", views.room_new, name="room_new"),
+    path("<str:room_pk>/chat/", views.room_chat, name="room_chat"),
 ]
